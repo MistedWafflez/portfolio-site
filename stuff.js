a2513c3e799c1ba8c885ab9cadd01c849c6ad36b
MistedWafflez <141043805+MistedWafflez@users.noreply.github.com>
2025-12-07 16:02:59 -0600

    Update stuff.js

--- stuff.js
+++ stuff.js
@@ -1,24 +1,53 @@
-function pingRetronetWeb() {
+function ping(url,onSuccess,onFailure) {
     var xhr;
     if (window.XMLHttpRequest) {
         xhr = new XMLHttpRequest();
     } else {
         xhr = new ActiveXObject("Microsoft.XMLHTTP");
     }
-    xhr.open('GET', '//www.retronet.win/example.json', true);
+    xhr.onreadystatechange = function() {
+        if (xhr.readyState === 4) {
+            if (xhr.status >= 200 && xhr.status < 300) {
+                onSuccess(xhr.responseText);
+            } else {
+                onFailure(xhr.status);
+            }
+        }
+    };
+    xhr.open('GET', url, true);
     xhr.send();
 }
 
-function pingRetronetChat() {
-    var xhr;
-    if (window.XMLHttpRequest) {
-        xhr = new XMLHttpRequest();
-    } else {
-        xhr = new ActiveXObject("Microsoft.XMLHTTP");
+var RetronetWebButton = document.getElementById('Button-RetroNetWeb');
+var RetronetWebStatus = document.getElementById('RetronetWeb-StatusText');
+var RetronetChatButton = document.getElementById('Button-RetroNetChat');
+var RetronetChatStatus = document.getElementById('RetronetM-StatusText');
+
+function assignText(element, value) {
+  if (!element) return;
+  if ('textContent' in element) {
+    element.textContent = value;
+  } else {
+    element.innerText = value;
+  }
+};
+
+ping('//www.retronet.win/example.json',
+    function success(data) {
+        assignText(RetronetWebStatus,'Online');
+    },
+    function failure(status) {
+        assignText(RetronetWebStatus,'Offline');
+        RetronetWebButton.removeAttribute("href");
     }
-    xhr.open('GET', '//chat.retronet.win/example.json', true);
-    xhr.send();
-}
+);
 
-pingRetronetWeb();
-pingRetronetChat();
+ping('//chat.retronet.win/example.json',
+    function success(data) {
+        assignText(RetronetChatStatus,'Online');
+    },
+    function failure(status) {
+        assignText(RetronetChatStatus,'Offline');
+        RetronetChatButton.removeAttribute("href");
+    }
+);
